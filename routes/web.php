12c4a2a3fcaf3909cd7183a4c1f37df544266ab3
t-sakurai816 <t.devsaku@gmail.com>
2022-10-11 00:03:19 +0900

    削除処理を追加

--- routes/web.php
+++ routes/web.php
@@ -32,3 +32,6 @@ Route::get('/tweet/update/{tweetId}', \App\Http\Controllers\Tweet\Update\IndexCo
 
 Route::put('/tweet/update/{tweetId}', \App\Http\Controllers\Tweet\Update\PutController::class)
 ->name('tweet.update.put');
+
+Route::delete('/tweet/delete/{tweetId}', \App\Http\Controllers\Tweet\DeleteController::class)
+->name('tweet.delete');
