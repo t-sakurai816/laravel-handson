ea4b09bcd2e26e5236940fb215564a9535783175
t-sakurai816 <t.devsaku@gmail.com>
2022-10-10 16:09:38 +0900

    データベースに接続して表示

--- resources/views/tweet/index.blade.php
+++ resources/views/tweet/index.blade.php
@@ -1,13 +1,20 @@
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>つぶやきアプリ</title>
 </head>
+
 <body>
     <h1>つぶやきアプリ</h1>
-    <p>{{ $name }}</p>
+    <div>
+        @foreach ($tweets as $tweet)
+            <p>{{ $tweet->content }}</p>
+        @endforeach
+    </div>
 </body>
+
 </html>
